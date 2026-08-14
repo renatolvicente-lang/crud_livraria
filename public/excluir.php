75641f6060a00d902996287178782a6132ca50e9
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-08-14 15:09:02 +0000

    up - adição de prepared statement no arquivo excluir.php

--- public/excluir.php
+++ public/excluir.php
@@ -2,6 +2,12 @@
 include "../infra/conexao.php";
 $id = $_GET["id"];
 $sql = "DELETE FROM livros WHERE id=$id";
-mysqli_query($conexao,$sql);
+
+$comando = $conexao -> prepare($sql);
+
+$comando -> bind_param('i', $id);
+
+$comando -> execute();
+
 header("Location: ../index.php");
 ?>
